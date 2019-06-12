12ff26b3d79d82266b691c79ccde66028f953364
cyc <cycallj@163.com>
2019-06-12 15:09:24 +0800

    success to publish as a react component

---
+++ src/components/SmallButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SmallButton extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<button>嘤嘤嘤</button>
+			</div>
+		)
+	}
+	
+}
+
+export default SmallButton;
